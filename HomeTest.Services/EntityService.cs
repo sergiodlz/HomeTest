cc30a08935807e9c777d497972d5a2a81fcf4c17
Sergio De La Hoz <sdelahoz@nimocom.com>
2020-10-17 15:44:53 -0500

    EntityRepository implementation

---
+++ HomeTest.Services/EntityService.cs
@@ -0,0 +1,80 @@
+﻿using HomeTest.Data;
+using HomeTest.Data.Entities;
+using HomeTest.Services.Core;
+using HomeTest.Services.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HomeTest.Services
+{
+    public class EntityService<TEntity> : EntityRepository<TEntity>, IEntityService<TEntity>
+        where TEntity : BaseEntity
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EntityService(ApplicationDbContext context)
+            : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<TEntity> CreateAsync(TEntity entity)
+        {
+            Create(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            Delete(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DisableAsync(TEntity entity)
+        {
+            entity.Enable = false;
+            Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> FindByConditionAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            return await FindByCondition(expression).Where(t => t.Enable).ToListAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await FindAll().ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(Guid id)
+        {
+            return await FindByCondition(t => t.Enable && t.Id.Equals(id)).FirstOrDefaultAsync();
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            Update(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<IEnumerable<TEntity>> FindByConditionAndIncludeAsync(Expression<Func<TEntity, bool>> expression,
+            params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            return await FindByConditionAndInclude(expression, includeProperties).Where(t => t.Enable).ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAndIncludeAsync(Guid id, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            return await FindByConditionAndInclude(t => t.Enable && t.Id.Equals(id), includeProperties).FirstOrDefaultAsync();
+        }
+    }
+}
---
+++ HomeTest.Services/Repository/EntityRepository.cs
@@ -0,0 +1,57 @@
+﻿using HomeTest.Data;
+using HomeTest.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace HomeTest.Services.Repository
+{
+    public abstract class EntityRepository<TEntity> : IEntityRepository<TEntity>
+        where TEntity : BaseEntity
+    {
+        protected ApplicationDbContext RepositoryContext { get; set; }
+
+        public EntityRepository(ApplicationDbContext repositoryContext)
+        {
+            RepositoryContext = repositoryContext;
+        }
+
+        public void Create(TEntity entity)
+        {
+            RepositoryContext.Set<TEntity>().Add(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            RepositoryContext.Set<TEntity>().Remove(entity);
+        }
+
+        public IQueryable<TEntity> FindAll()
+        {
+            return RepositoryContext.Set<TEntity>().AsNoTracking();
+        }
+
+        public IQueryable<TEntity> FindByCondition(Expression<Func<TEntity, bool>> expression)
+        {
+            return RepositoryContext.Set<TEntity>().Where(expression).AsNoTracking();
+        }
+
+        public IQueryable<TEntity> FindByConditionAndInclude(Expression<Func<TEntity, bool>> expression, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = RepositoryContext.Set<TEntity>().Where(expression).AsNoTracking();
+
+            foreach (var includeProperty in includeProperties)
+            {
+                query = query.Include(includeProperty);
+            }
+
+            return query;
+        }
+
+        public void Update(TEntity entity)
+        {
+            RepositoryContext.Set<TEntity>().Update(entity);
+        }
+    }
+}
