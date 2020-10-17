6c37b92e2062aa1732003f0ba8a0f0aa7ce1d295
Sergio De La Hoz <sdelahoz@nimocom.com>
2020-10-17 13:12:58 -0500

    Agregar archivos de proyecto.

---
+++ HomeTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HomeTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
