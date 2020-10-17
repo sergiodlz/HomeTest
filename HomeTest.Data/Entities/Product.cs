fc573d6fbaf274a45a8c747036286c422bd127b6
Sergio De La Hoz <sdelahoz@nimocom.com>
2020-10-17 17:27:56 -0500

    Adding validations to model

--- HomeTest.Data/Entities/Product.cs
+++ HomeTest.Data/Entities/Product.cs
@@ -1,7 +1,11 @@
-﻿namespace HomeTest.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeTest.Data.Entities
 {
     public class Product : BaseEntity
     {
+        [Required]
+        [MinLength(3)]
         public string Name { get; set; }
 
         public string Description { get; set; }
