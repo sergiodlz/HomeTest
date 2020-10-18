4223664cf9fe268637bba4c0d88c1c494bf2498e
Sergio De La Hoz <sdelahoz@nimocom.com>
2020-10-18 16:01:46 -0500

    - Azure deploy - Ensure db creation

--- HomeTest/Startup.cs
+++ HomeTest/Startup.cs
@@ -31,7 +31,7 @@ namespace HomeTest
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext _context)
         {
             if (env.IsDevelopment())
             {
@@ -44,6 +44,9 @@ namespace HomeTest
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            _context.Database.EnsureCreated();
+
             app.UseCors(x => x
                 .AllowAnyOrigin()
                 .AllowAnyMethod()
